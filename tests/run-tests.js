3524ecb8d912bcc33d08aad57875a9ef69200b7b
Keith Lazuka <kelazuka@microsoft.com>
2018-07-02 18:49:49 -0700

    Make the integration test do hot-module-reload

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -1,4 +1,5 @@
-
+const path = require('path');
+const fs = require('fs');
 const chai = require("chai");
 const expect = chai.expect;
 const puppeteer = require('puppeteer');
@@ -9,14 +10,13 @@ const serverUrl = 'http://127.0.0.1:3000';
 const counterIncrementId = "#button-plus";
 const counterValueId = "#counter-value";
 
-process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', p);
-    process.exit(1);
-});
-
+const pathToElmCode = path.join(__dirname, "../app/dist/elm-output.js");
+const elmCode = fs.readFileSync(pathToElmCode, {encoding: "utf8"});
+const originalIncrementCode = "return _Utils_Tuple2(model + 1, elm$core$Platform$Cmd$none);";
+const modifiedIncrementCode = originalIncrementCode.replace("model + 1", "model + 100");
 
 (async() => {
-    const browser = await puppeteer.launch({headless: false, slowMo: 100});
+    const browser = await puppeteer.launch({headless: true, slowMo: 100});
     const page = await browser.newPage();
 
     console.log("Loading the page");
@@ -27,9 +27,24 @@ process.on('unhandledRejection', (reason, p) => {
     await incrementCounter(page);
     expect(await getCounterValue(page)).to.equal(1);
 
+    console.log("Modifying the Elm code to change the increment-by amount");
+    fs.writeFileSync(pathToElmCode, elmCode.replace(originalIncrementCode, modifiedIncrementCode));
+    await timeout(1000);
+
+    expect(await getCounterValue(page)).to.equal(1);
+    await incrementCounter(page);
+    expect(await getCounterValue(page)).to.equal(101);
+
     console.log("Closing the browser");
     await browser.close();
-})();
+})().catch((reason) => {
+    console.log('Unhandled promise failure:', reason);
+}).finally(() => {
+    console.log("Final cleanup");
+    fs.writeFileSync(pathToElmCode, elmCode);
+});
+
+const timeout = ms => new Promise(res => setTimeout(res, ms));
 
 async function incrementCounter(page) {
     console.log("Incrementing the counter");
