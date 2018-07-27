3ef12ad0eb1552f6d6e0021972fc638cd95d840b
Keith Lazuka <kelazuka@microsoft.com>
2018-07-27 14:03:39 -0700

    Add example code that uses async processes

---
+++ test/fixtures/MainWithTasks.elm
@@ -0,0 +1,61 @@
+module MainWithTasks exposing (..)
+
+import Browser
+import Html exposing (button, div, h1, p, span, text)
+import Html.Attributes exposing (id)
+import Html.Events exposing (onClick)
+import Process
+import Task
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+type alias Model =
+    { count : Int }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { count = 0 }, Cmd.none )
+
+
+type Msg
+    = Increment
+    | FinishedSleeping (Result String ())
+
+
+update msg model =
+    case Debug.log "msg" msg of
+        Increment ->
+            ( model
+            , Task.attempt FinishedSleeping (Process.sleep 10000)
+            )
+
+        FinishedSleeping result ->
+            case result of
+                Ok _ ->
+                    ( { model | count = model.count + 1 }
+                    , Cmd.none
+                    )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+
+view model =
+    div []
+        [ h1 [] [ text "MainWithTasks" ]
+        , span [ id "code-version" ] [ text "code: v5" ]
+        , p []
+            [ text "Counter value is: "
+            , span [ id "counter-value" ] [ text (String.fromInt model.count) ]
+            ]
+        , button [ onClick Increment, id "counter-button" ] [ text "+ (delayed)" ]
+        ]
