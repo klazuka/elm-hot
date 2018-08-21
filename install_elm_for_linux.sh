f3f2f59ebe8f4978b2b1c8be30b4baf25e6f399a
Keith Lazuka <klazuka@gmail.com>
2018-08-21 06:48:11 -0700

    Install the final build of Elm 0.19 for Linux (#3)

--- install_elm_for_linux.sh
+++ install_elm_for_linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-curl https://44a95588fe4cc47efd96-ec3c2a753a12d2be9f23ba16873acc23.ssl.cf2.rackcdn.com/linux-64.tar.gz > elm19.tar.gz
+# remove this script once the npm Elm 0.19 installer is released
+curl https://44a95588fe4cc47efd96-ec3c2a753a12d2be9f23ba16873acc23.ssl.cf2.rackcdn.com/binaries-for-linux-64.tar.gz > elm19.tar.gz
 tar zxvf elm19.tar.gz
 rm elm19.tar.gz
 mv elm node_modules/.bin/elm
