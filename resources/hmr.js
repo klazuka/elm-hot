a13664080cfeb71bb5b4f8c57fcd61e238dd4af8
Keith Lazuka <klazuka@gmail.com>
2019-11-04 11:23:12 -0500

    Fix a crash when using Elm debugger and elm/browser 1.0.2 (#34)
    * Guard against a crash when searching for Browser.Navigation.Key
    
    I'm not sure why this is allowed, but `elm/browser` version 1.0.2
    has internal JS data where some "popout" fields contain the
    value `undefined`. So when we do the breadth-first traversal of
    the object model, it's important that we skip over them.
    
    * Detect Elm debugger internals in elm/browser 1.0.2

--- resources/hmr.js
+++ resources/hmr.js
@@ -356,6 +356,10 @@ if (module.hot) {
             while (queue.length !== 0) {
                 var item = queue.shift();
 
+                if (typeof item.value === "undefined") {
+                    continue;
+                }
+
                 // The nav key is identified by a runtime tag added by the elm-hot injector.
                 if (item.value.hasOwnProperty("elm-hot-nav-key")) {
                     // found it!
@@ -379,7 +383,11 @@ if (module.hot) {
 
 
         function isDebuggerModel(model) {
-            return model && model.hasOwnProperty("expando") && model.hasOwnProperty("state");
+            // Up until elm/browser 1.0.2, the Elm debugger could be identified by a
+            // property named "expando". But in version 1.0.2 that was renamed to "expandoModel"
+            return model
+                && (model.hasOwnProperty("expando") || model.hasOwnProperty("expandoModel"))
+                && model.hasOwnProperty("state");
         }
 
         function getAt(keyPath, obj) {
